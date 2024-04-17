7b531bad86a6fc509b5c31f63722cee27ee4cf02
Nazlin <jemelinazlin@gmail.com>
2024-04-17 14:23:31 +0300

    fetch function

--- index.js
+++ index.js
@@ -3,4 +3,16 @@ document.addEventListener("DOMContentLoaded", () => {
   fetchRestaurants(url);
 });
 
-
+function fetchRestaurants(url) {
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      data.forEach((restaurant) => {
+        displayRestaurant(restaurant);
+      });
+    })
+    .catch((error) => {
+      console.error("Error fetching restaurants:", error);
+    });
+  }
